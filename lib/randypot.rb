28dd3a392f311e584867af41fa743d2843754584
Fernando García Samblas <fernando.garcia@the-cocktail.com>
2009-12-21 00:14:57 +0100

    Randypot.creation (take one)

--- lib/randypot.rb
+++ lib/randypot.rb
@@ -13,9 +13,29 @@ class Randypot
     def config
       @config ||= Config.new
     end
-    
+
     def configure(config_file = nil, &block)
       config.configure config_file, &block
+    end
+
+    def activities_url
+      config.service_url + '/activities/'
+    end
+
+    def creation(base_params = nil)
+      MagicParams.abracadabra(
+       [:content_type, :content_source], 
+       base_params) do |params|
+        params[:activity_type] = 'creation'
+        params[:activity_at] = Time.now
+        connection.post activities_url, ParamsTransformer.transform(params)
+      end
+    end
+
+    private
+    def connection
+      @connection ||= Connection.new(
+        config.service_url, config.app_key, config.app_token)
     end
   end
 end
--- spec/lib/randypot_spec.rb
+++ spec/lib/randypot_spec.rb
@@ -1,31 +1,74 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '../spec_helper'))
 
 describe Randypot do
-  it ".config should be the same object for every call" do
+  it '.config should be the same object for every call' do
     Randypot.config.should == Randypot.config
   end
 
-  it ".configure should configure the .config object" do
-    Randypot.config.should_receive(:foo).with(:bar)
-    Randypot.configure do |c|
-      c.foo :bar
-    end
+  it 'should build the url for posting new activities using config.service_url' do
+    Randypot.should_receive(:config).and_return(
+      mock('config', :service_url => 'http://...'))
+    Randypot.activities_url.should == 'http://.../activities/'
   end
-  
-  it ".new should let us configure the .config object as well" do
-    Randypot.config.should_receive(:foo).with(:bar)
-    Randypot.new do |c|
-      c.foo :bar
+    
+  describe 'configuration methods' do
+    describe '.configure' do
+      it 'should configure the .config object' do
+        Randypot.config.should_receive(:foo).with(:bar)
+        Randypot.configure do |c|
+          c.foo :bar
+        end
+      end
+      
+      it 'should use the given YAML' do
+        Randypot.config.should_receive(:configure).with('myconf.yml')
+        Randypot.new 'myconf.yml'
+      end
     end
+
+    describe '.new' do
+      it 'should configure the .config object' do
+        Randypot.config.should_receive(:foo).with(:bar)
+        Randypot.new do |c|
+          c.foo :bar
+        end
+      end
+    
+      it 'should use the given YAML' do
+        Randypot.config.should_receive(:configure).with('myconf.yml')
+        Randypot.new 'myconf.yml'
+      end
+   end
   end
 
-  it ".configure should use the given YAML" do
-    Randypot.config.should_receive(:configure).with('myconf.yml')
-    Randypot.new 'myconf.yml'
-  end
+  describe '.creation' do
+    before do
+      @base_params = {
+        :content => 'http://example.com/wadus.png',
+        :content_type => 'wadus',
+        :content_source => 'ugc'
+      }
+      conn, now, transformed_params = mock('Connection'), Time.now, {}
+      Time.stub!(:now).and_return(now)
+      Randypot.should_receive(:connection).and_return(conn)
+      Randypot::ParamsTransformer.should_receive(:transform).with(
+        @base_params.merge({
+        :activity_type => 'creation',
+        :activity_at => now
+      })).and_return(transformed_params)
+      conn.should_receive(:post).with(Randypot.activities_url, transformed_params)
+    end
 
-  it ".new should use the given YAML" do
-    Randypot.config.should_receive(:configure).with('myconf.yml')
-    Randypot.new 'myconf.yml'
+    it 'should create from passed params' do
+      Randypot.creation(@base_params)
+    end
+
+    it 'should pass its first nested method as :content_type' do
+      Randypot.creation.wadus(@base_params.reject{|k,v| k == :content_type})
+    end
+
+    it 'should pass its second nested method as :content_source' do
+      Randypot.creation.wadus.ugc(:content => @base_params[:content])
+    end
   end
 end
