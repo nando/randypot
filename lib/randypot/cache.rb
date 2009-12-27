d81779cf3ef51e97e2f09ae1cce5e512608319dc
Fernando García Samblas <fernando.garcia@the-cocktail.com>
2009-12-27 01:32:04 +0100

    Randypot::Cache

--- lib/randypot/cache.rb
+++ lib/randypot/cache.rb
@@ -1,12 +1,18 @@
 require 'tmpdir'
+require 'yaml'
 require 'digest/md5'
 
 class Randypot
   class Cache
     class << self
       def get(key)
-        if File.file?(filepath(key))
+        if File.file?(file = filepath(key))
+          YAML.load(file)
         end
+      end
+
+      def put(key, object)
+        File.open(filepath(key), 'w').write(object.to_yaml)
       end
   
       private
--- spec/lib/randypot_cache_spec.rb
+++ spec/lib/randypot_cache_spec.rb
@@ -2,26 +2,28 @@ require File.expand_path(File.join(File.dirname(__FILE__), '../spec_helper'))
 
 describe Randypot::Cache do
     before do
-      @key = 'güadus'
-      @digest = Digest::MD5.hexdigest('güadus')
-      @response_body = <<MEMBERS_RESPONSE
-0318254ce7c576c583e8a63558e7f98877e6ec49,12,2009-08-03 20:46:40 UTC
-059fc5c60729cad3bfe07e1a5edaa51104c14518,12,2009-08-03 20:46:40 UTC
-0699d11f05594b85c6004ebba26712a5f9940090,12,2009-08-03 20:46:40 UTC
-0f4d89c1a93ea1b314ec6cad9a07cb743e41953c,13,2009-08-03 20:46:40 UTC
-MEMBERS_RESPONSE
+      @key = 'members'
+      Dir.should_receive(:tmpdir).and_return('/tmp')
+      @filepath = '/tmp/randypot/' + Digest::MD5.hexdigest('members')
     end
 
     it '.get should return nil if cache file does not exist' do
-      Digest::MD5.should_receive(:hexdigest).with(@key).and_return(@digest)
-      Dir.should_receive(:tmpdir).and_return('')
-      File.should_receive(:file?).with('/randypot/' + @digest).and_return(false)
+      File.should_receive(:file?).with(@filepath).and_return(false)
       Randypot::Cache.get(@key).should be_nil
     end
 
-#    it '.put' do
-#      File.should_receive(:open).with(Dir.tmpdir + '/randypot/members_cache', 'w').and_yield(file)
-#
-#      Randypot::Cache.get @key
-#    end
+    it '.get should return Response if cache file does exist' do
+      response = mock('response')
+      File.should_receive(:file?).with(@filepath).and_return(true)
+      YAML.should_receive(:load).with(@filepath).and_return(response)
+      Randypot::Cache.get(@key).should == response
+    end
+
+    it '.put' do
+      file, response, res_to_yaml = mock('file'), mock('response'), mock('res_to_yaml')
+      File.should_receive(:open).with(@filepath, 'w').and_return(file)
+      response.should_receive(:to_yaml).and_return(res_to_yaml)
+      file.should_receive(:write).with(res_to_yaml)
+      Randypot::Cache.put @key, response
+    end
 end
