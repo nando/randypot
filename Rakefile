a072f8464654000fa54b97c8c2ff0c033abfcbbc
Fernando García Samblas <fernando.garcia@the-cocktail.com>
2009-12-29 02:09:32 +0100

    Integration testing started

--- Rakefile
+++ Rakefile
@@ -5,5 +5,5 @@ task :default => :spec
 
 desc "Run all specs in spec directory (excluding plugin specs)"
 Spec::Rake::SpecTask.new(:spec) do |t|
-  t.spec_files = FileList['spec/**/*/*_spec.rb']
+  t.spec_files = FileList['spec/lib/*_spec.rb']
 end
---
+++ spec/integration/randypot_spec.rb
@@ -0,0 +1,55 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '../spec_helper'))
+
+Randypot.configure do |config|
+  config.service_url = 'http://localhost.localdomain.lan:3000/api'
+  config.app_key = '1' 
+  config.app_token = '1' 
+end
+
+def should_return_status(status)
+  yield.status.should be(status)
+end
+
+describe Randypot do
+  it '.creation' do
+    should_return_status(201) do 
+      Randypot.creation(
+        :content_source => 'ugc',
+        :content_type => 'default',
+        :content => 'http://example.com/foo.html',
+        :member => 'randy@example.com')
+    end
+  end
+  it '.creation.default' do
+    should_return_status(201) do 
+      Randypot.creation.default(
+        :content_source => 'ugc',
+        :content => 'http://example.com/foo.html',
+        :member => 'randy@example.com')
+    end
+  end
+  it '.creation.default.ugc' do
+    should_return_status(201) do 
+      Randypot.creation.default.ugc(
+        :content => 'http://example.com/foo.html',
+        :member => 'randy@example.com')
+    end
+  end
+  it '.creation.default.editorial' do
+    should_return_status(201) do 
+      Randypot.creation.default.editorial(
+        :content => 'http://example.com/foo.html',
+        :member => 'randy@example.com')
+    end
+  end
+  it '.members' do
+    should_return_status(200) do 
+      Randypot.members
+    end
+  end
+  #it '.member(id)' do      
+  #  should_return_status(200) do 
+  #    Randypot.member('randy@example.com')
+  #  end
+  #end
+end
