b9882d0bf3ebab46527b8eee3d1ff3c24a4f1a3c
Fernando García Samblas <fernando.garcia@the-cocktail.com>
2009-12-30 14:04:50 +0100

    Reactions' integration tests

--- spec/integration/randypot_spec.rb
+++ spec/integration/randypot_spec.rb
@@ -13,46 +13,75 @@ def should_return_status(status)
 end
 
 describe Randypot do
-  it '.creation' do
-    should_return_status(201) do 
-      Randypot.creation(
-        :content_source => 'ugc',
-        :content_type => 'default',
-        :content => 'http://example.com/foo.html',
-        :member => 'randy@example.com')
+  describe 'activities' do
+    after do
+      @res.status.should be(201)
     end
-  end
-  it '.creation.default' do
-    should_return_status(201) do 
-      Randypot.creation.default(
-        :content_source => 'ugc',
-        :content => 'http://example.com/foo.html',
-        :member => 'randy@example.com')
+    it '.creation' do
+      @res = Randypot.creation(
+          :content_source => 'ugc',
+          :content_type => 'default',
+          :content => 'http://example.com/foo.html',
+          :member => 'randy@example.com')
     end
-  end
-  it '.creation.default.ugc' do
-    should_return_status(201) do 
-      Randypot.creation.default.ugc(
-        :content => 'http://example.com/foo.html',
-        :member => 'randy@example.com')
+    it '.creation.default' do
+      @res = Randypot.creation.default(
+          :content_source => 'ugc',
+          :content => 'http://example.com/foo.html',
+          :member => 'randy@example.com')
     end
-  end
-  it '.creation.default.editorial' do
-    should_return_status(201) do 
-      Randypot.creation.default.editorial(
-        :content => 'http://example.com/foo.html',
-        :member => 'randy@example.com')
+    it '.creation.default.ugc' do
+      @res = Randypot.creation.default.ugc(
+          :content => 'http://example.com/foo.html',
+          :member => 'randy@example.com')
+    end
+    it '.creation.default.editorial' do
+      @res = Randypot.creation.default.editorial(
+          :content => 'http://example.com/foo.html',
+          :member => 'randy@example.com')
+    end
+    it '.reaction' do
+      @res = Randypot.reaction(
+          :member => 'friend@example.com',
+          :category => 'default',
+          :content_source => 'ugc',
+          :content_type => 'default',
+          :content => 'http://example.com/foo.html',
+          :member_b => 'randy@example.com')
+    end
+    it '.reaction.default' do
+      @res = Randypot.reaction.default(
+          :member => 'friend@example.com',
+          :content_source => 'ugc',
+          :content_type => 'default',
+          :content => 'http://example.com/foo.html',
+          :member_b => 'randy@example.com')
+    end
+    it '.reaction.default.default' do
+      @res = Randypot.reaction.default.default(
+          :member => 'friend@example.com',
+          :content_source => 'ugc',
+          :content => 'http://example.com/foo.html',
+          :member_b => 'randy@example.com')
+    end
+    it '.reaction.default.default.editorial' do
+      @res = Randypot.reaction.default.default.editorial(
+          :member => 'friend@example.com',
+          :content => 'http://example.com/foo.html',
+          :member_b => 'randy@example.com')
+    end
+    it '.reaction.default.default.ugc' do
+      @res = Randypot.reaction.default.default.ugc(
+          :member => 'friend@example.com',
+          :content => 'http://example.com/foo.html',
+          :member_b => 'randy@example.com')
     end
   end
   it '.members' do
-    should_return_status(200) do 
-      Randypot.members
-    end
+    Randypot.members.status.should be(200)
   end
   it '.members from cache' do
-    should_return_status(200) do 
-      Randypot.members
-    end
+    Randypot.members.status.should be(200)
   end
   #it '.member(id)' do      
   #  should_return_status(200) do 
